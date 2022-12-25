c4ce42206dfe57c6077e4d26825949df11a578a4
Wayne Myers <wgmyers@gmail.com>
2022-12-25 20:48:51 +0000

    Initial commit

---
+++ lib/cal.rb
@@ -0,0 +1,15 @@
+# cal.rb
+
+# A library to handle standard cal output
+class Calendar
+
+  def initialize(options)
+    @options = options
+  end
+
+  def print
+    puts "Hi mum!"
+    pp @options
+  end
+
+end
