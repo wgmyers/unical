37e2dad1a4d8e04eae08eaeb232e048b45b4d7e0
Wayne Myers <wgmyers@gmail.com>
2022-12-27 03:13:16 +0000

    Fix rubocop complaints

--- test/test_output.rb
+++ test/test_output.rb
@@ -10,15 +10,15 @@
 require 'pty'
 
 TESTS = [
-  ["", "No flags given"],
-  ["-h", "Remove highlighting"], # FAILS b/c cal -h does not behave as expected.
-  ["07 1971", "July 1971"],
-  ["-m 6", "June of this year"],
-  ["-1", "One month only"],
-  ["-3", "Three months with current month in middle"],
-  ["1984", "Year specified without -y"],
-  ["-y 1984", "Year specified with -y"],
-  ["-d 1984-12", "Month specified with -d"]
+  ['', 'No flags given'],
+  ['-h', 'Remove highlighting'], # FAILS b/c cal -h does not behave as expected.
+  ['07 1971', 'July 1971'],
+  ['-m 6', 'June of this year'],
+  ['-1', 'One month only'],
+  ['-3', 'Three months with current month in middle'],
+  ['1984', 'Year specified without -y'],
+  ['-y 1984', 'Year specified with -y'],
+  ['-d 1984-12', 'Month specified with -d']
 ]
 
 # strip_trailing
@@ -43,12 +43,12 @@ end
 def compare_output(flags)
   cal_cmd = "cal #{flags}"
   unical_cmd = "../unical.rb #{flags}"
-  #cal_output = strip_trailing(`#{cal_cmd}`)
+  # cal_output = strip_trailing(`#{cal_cmd}`)
   # We need to run cal from a PTY or it won't do highlighting
   cal_output_lines = []
   begin
-    PTY.spawn(cal_cmd) do |r_f,w_f,pid|
-        r_f.each {|line| cal_output_lines.push(line) }
+    PTY.spawn(cal_cmd) do |r_f, _w_f, _pid|
+      r_f.each { |line| cal_output_lines.push(line) }
     end
   rescue Errno::EIO
   end
@@ -56,17 +56,18 @@ def compare_output(flags)
   unical_output = strip_trailing(`#{unical_cmd}`)
 
   return true if cal_output == unical_output
+
   false
 end
 
-puts "Testing output of unical.rb against cal output:"
-puts "NB: the -h test fails because cal -h does not work as expected"
+puts 'Testing output of unical.rb against cal output:'
+puts 'NB: the -h test fails because cal -h does not work as expected'
 
 TESTS.each do |test|
   print "* - #{test[1]} (#{test[0]}): "
   if compare_output(test[0])
-    puts "OK"
+    puts 'OK'
   else
-    puts "FAILED"
+    puts 'FAILED'
   end
 end
