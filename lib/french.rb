7331025dc230ac4524163ca297fa1ce7bcbc6a5e
Wayne Myers <wgmyers@gmail.com>
2022-12-29 02:03:31 +0000

    Handle sansculottides in year output

--- lib/french.rb
+++ lib/french.rb
@@ -7,6 +7,33 @@ require 'RevCal'
 # Handle cal-like output for French Revolutionary calendar
 class French < Calendar
 
+  # print_year
+  # Print a full year
+  # We need to override the default to handle Sansculottides
+  def print_year
+    puts "An #{@options[:year]}".center(92)
+    (2..11).step(3) do |m|
+      @options[:month] = m
+      print_three_months
+      puts ''
+    end
+    # Print the Sansculottides for this year
+    @options[:month] = 13
+    print_month
+  end
+
+
+  # leap_year
+  # NB: Year must be a French Revolutionary year
+  # We use the method from the library, which does the same thing as Emacs does,
+  # which is to pretty much use the Gregorian method except for the historical
+  # leap years of 3, 7, 11, 15, and 20
+  def leap_year?(year)
+    return true if RevDate.length(year) == 366
+
+    false
+  end
+
   def calc_month(year, month)
     output = []
 
@@ -39,25 +66,38 @@ class French < Calendar
       output.push("#{pretty_month} #{year}".center(30))
     end
 
-    # Add days of week
-    # FIXME: this is a non-ideal way of doing two-letter forms of the ten day
-    # week, but I'm not sure that Qa Qi Sx and Sp are any better.
-    # At least we don't need to worry about internationalisation here.
-    output.push('Pr Du Tr Qu Qu Se Se Oc No Dé')
+    if month == 13
+      # Handle Sanculottides here
+      leap_days = leap_year?(year) ? 6 : 5
 
-    line = ''
-    (1..30).each do |d|
-      line += if @options[:highlight] == true &&
-                 today.year == ref_d.year &&
-                 today.month == ref_d.month &&
-                 today.day == d
-                "\033[7m#{d.to_s.rjust(2, ' ')}\033[27m "
-              else
-                "#{d.to_s.rjust(2, ' ')} "
-              end
-      if (d % 10).zero?
-        output.push(line)
-        line = ''
+      (1..leap_days).each do |d|
+        sansculotte = RevDate.new(ref_d.year, ref_d.month, d)
+        output.push(sansculotte.daySymbol)
+      end
+
+    else
+      # A 'normal' month
+
+      # Add days of week
+      # FIXME: this is a non-ideal way of doing two-letter forms of the ten day
+      # week, but I'm not sure that Qa Qi Sx and Sp are any better.
+      # At least we don't need to worry about internationalisation here.
+      output.push('Pr Du Tr Qu Qu Se Se Oc No Dé ')
+
+      line = ''
+      (1..30).each do |d|
+        line += if @options[:highlight] == true &&
+                   today.year == ref_d.year &&
+                   today.month == ref_d.month &&
+                   today.day == d
+                  "\033[7m#{d.to_s.rjust(2, ' ')}\033[27m "
+                else
+                  "#{d.to_s.rjust(2, ' ')} "
+                end
+        if (d % 10).zero?
+          output.push(line)
+          line = ''
+        end
       end
     end
 
